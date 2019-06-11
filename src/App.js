cfce71b2c80181be0a5d744ffe1b53c0d73e8c30
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-10 23:14:22 -0300

    38. Cómo transformar un functional en un class Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import WeatherLocation from './components/WeatherLocation';
-
-
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <WeatherLocation></WeatherLocation>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <WeatherLocation></WeatherLocation>
+          </div>
+      );
+  }
 }
 
 export default App;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData/index';
 import './styles.css';
@@ -13,11 +13,15 @@ const data = {
     wind: '10 m/s',
 };
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={'Barcelona'}></Location>
-        <WeatherData data={data}></WeatherData>
-    </div>
-);
+class WeatherLocation extends Component {
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={'Barcelona'}></Location>
+                <WeatherData data={data}></WeatherData>
+            </div>
+        );
+    }
+}
 
 export default WeatherLocation;
