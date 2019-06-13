5921cb479cbc2e026b43d3d91a79d26b1d42fb4e
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-12 21:49:46 -0300

    48. Object Literal Property Value Shorthand

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,20 +4,18 @@ import WeatherData from './WeatherData/index';
 import './styles.css';
 import {
     SUN,
-    WINDY,
 } from '../../constants/weathers';
+
+const location = 'Pato Branco,br';
+const api_key = 'f99bbd9e4959b513e9bd0d7f7356b38d';
+const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather';
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
     temperature: 7,
     weatherState: SUN,
     humidity: 10,
-    wind: '10 m/s',
-};
-
-const data2 = {
-    temperature: 5,
-    weatherState: WINDY,
-    humidity: 20,
     wind: '10 m/s',
 };
 
@@ -31,12 +29,38 @@ class WeatherLocation extends Component {
         };
     }
 
-    handleUpdateClick = () => {
-        console.log('actualizado');
+    getWeatherState = weather_data => {
+        return SUN;
+    };
 
-        this.setState( {
-            city: 'Buenos Aires!',
-            data: data2,
+    getCity = weather_data => weather_data.name;
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        };
+
+        return data;
+    };
+
+    handleUpdateClick = () => {
+        fetch(api_weather).then(resolve => {
+            return resolve.json();
+        }).then(data => {
+            const city = this.getCity(data);
+            const newWeather = this.getData(data);
+            console.log(data);
+            this.setState({
+                city,
+                data: newWeather
+            });
         });
     };
 
